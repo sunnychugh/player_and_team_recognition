f5db17c6e3d9fb4742b6627dde74735d761083b0
Sunny Chugh <sunnychugh87@gmail.com>
2021-02-27 01:31:56 +0000

    Predict and color image as per team and player

--- prediction.py
+++ prediction.py
@@ -62,3 +62,45 @@ class Prediction:
             print(image_path)
             self.predict_image(image, predict_flag=True)
 
+    def predict_and_color_section_of_images(self, csv_file_dir):
+        for csv_file in glob.glob(csv_file_dir + "\\4.csv"):
+            print(csv_file)
+
+            image_path = csv_file[:-3] + "jpg"
+            invalid = check_for_invalid_image(image_path)
+            if invalid[0]:
+                print(f"faulty image ({image_path})...")
+                print(invalid[1])
+                continue
+
+            image = cv2.imread(str(image_path))
+            output_team = image.copy()
+            output_player_no = image.copy()
+            with open(csv_file, "r") as f:
+                f.readline()
+
+                for line in f:
+                    line = tuple(int(round(float(i))) for i in line.split(","))
+                    tl = line[:2]
+                    br = line[2:]
+
+                    # ROI = image[y1:y2, x1:x2]
+                    image_roi = image[tl[1] : br[1], tl[0] : br[0]]
+                    # Predict the labels for team and player_no
+                    team, player_no = self.predict_image(image_roi, predict_flag=True)
+
+                    # Initialize black image of same dimensions for drawing the rectangles
+                    blk = np.zeros(image.shape, np.uint8)
+                    # Draw rectangles
+                    color_index = self.colors[str(team % len(self.colors))]
+                    cv2.rectangle(blk, tl, br, color_index, cv2.FILLED)
+                    color_index = self.colors[str(player_no % len(self.colors))]
+                    cv2.rectangle(blk, tl, br, color_index, cv2.FILLED)
+                    # Generate result by blending both images (opacity is 0.25 = 25 %)
+                    output_team = cv2.addWeighted(output_team, 1.0, blk, 0.5, 1)
+                    output_player_no = cv2.addWeighted(
+                        output_player_no, 1.0, blk, 0.5, 1
+                    )
+
+                cv2.imwrite(image_path[:-4] + "_team.jpg", output_team)
+                cv2.imwrite(image_path[:-4] + "_player_no.jpg", output_player_no)
